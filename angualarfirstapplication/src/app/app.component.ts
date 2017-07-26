bf5c010db8438a12da117583b73fb2f941441cbf
hari krisha <hari.k.krishna4@gmail.com>
2017-07-26 13:03:28 -0500

    worked on services

--- angualarfirstapplication/src/app/app.component.ts
+++ angualarfirstapplication/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {MailService} from "./mail.service";
 
 @Component({
   selector: 'app-root',
   template: `
   <div>
     <app-simple-form></app-simple-form>
+    {{mail.message}}
   </div>`,
 })
 export class AppComponent {
   title = 'this is the first app ' ;
+  constructor( private mail:MailService){
+
+  }
 }
---
+++ angualarfirstapplication/src/app/mail.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MailService {
+   message = 'You have got mail';
+  constructor() { }
+
+}
