3c9659d5b3a5538004cea468d6ab252db4a310b7
hari krisha <hari.k.krishna4@gmail.com>
2017-07-30 00:46:23 -0500

    used the multiple classes in using ngClass

--- kudvenkat/src/app/app.component.ts
+++ kudvenkat/src/app/app.component.ts
@@ -6,8 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  pageHeader: string = 'Employee Header';
-  isDisabled : boolean = false;
-  badHtml: string = 'Hi <script> alert("Hacked") </script> world'
+classesToApply: string = 'italicsClass boldClass';
+  applyBoldClass: boolean = true;
+  applyColorClass: boolean = true;
+  addClasses(){
+    let classes = {
+      boldClass : this.applyBoldClass,
+      colorClass : this.applyColorClass
+
+    };
+    return classes;
+  }
+
 }
