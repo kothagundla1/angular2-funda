7169fca71501014ea957fcf026c45a1ec6747b3f
hari krisha <hari.k.krishna4@gmail.com>
2017-07-30 17:11:57 -0500

    used the pipes

--- kudvenkat/src/app/employee-list/employee-list.component.ts
+++ kudvenkat/src/app/employee-list/employee-list.component.ts
@@ -12,19 +12,19 @@ export class EmployeeListComponent implements OnInit {
     this.employees = [
       {
         code: 'emp101', name: 'Tom', gender: 'Male',
-        annualSalary: 5500, dateOfBirth: '25/6/1988'
+        annualSalary: 5500, dateOfBirth: '06/25/1988'
       },
       {
         code: 'emp102', name: 'Alex', gender: 'Male',
-        annualSalary: 5700.95, dateOfBirth: '9/6/1982'
+        annualSalary: 5700.95, dateOfBirth: '09/06/1982'
       },
       {
         code: 'emp103', name: 'Mike', gender: 'Male',
-        annualSalary: 5900, dateOfBirth: '12/8/1979'
+        annualSalary: 5900, dateOfBirth: '08/12/1979'
       },
       {
         code: 'emp104', name: 'Mary', gender: 'Female',
-        annualSalary: 6500.826, dateOfBirth: '14/10/1980'
+        annualSalary: 6500.826, dateOfBirth: '10/14/1980'
       },
     ];
   }
@@ -33,23 +33,23 @@ export class EmployeeListComponent implements OnInit {
     this.employees = [
       {
         code: 'emp101', name: 'Tom', gender: 'Male',
-        annualSalary: 5500, dateOfBirth: '25/6/1988'
+        annualSalary: 5500, dateOfBirth: '06/25/1988'
       },
       {
         code: 'emp102', name: 'Alex', gender: 'Male',
-        annualSalary: 5700.95, dateOfBirth: '9/6/1982'
+        annualSalary: 5700.95, dateOfBirth: '09/06/1982'
       },
       {
         code: 'emp103', name: 'Mike', gender: 'Male',
-        annualSalary: 5900, dateOfBirth: '12/8/1979'
+        annualSalary: 5900, dateOfBirth: '08/12/1979'
       },
       {
         code: 'emp104', name: 'Mary', gender: 'Female',
-        annualSalary: 6500.826, dateOfBirth: '14/10/1980'
+        annualSalary: 6500.826, dateOfBirth: '10/14/1980'
       },
       {
         code: 'emp105', name: 'Nancy', gender: 'Female',
-        annualSalary: 6700.826, dateOfBirth: '15/12/1982'
+        annualSalary: 6700.826, dateOfBirth: '12/12/1982'
       },
     ];
   }
