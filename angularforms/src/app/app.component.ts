60a16b98b652ffae814a712a817b3d14bcb6f4f4
hari krisha <hari.k.krishna4@gmail.com>
2017-07-31 16:20:21 -0500

    working with the radio buttons

--- angularforms/src/app/app.component.ts
+++ angularforms/src/app/app.component.ts
@@ -1,8 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
-import {Observable} from "rxjs/Observable";
-import "rxjs/add/observable/combineLatest";
-import "rxjs/add/operator/filter";
-
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +6,6 @@ import "rxjs/add/operator/filter";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('formRef') form;
-  name = 'hari';
-  onSubmit(formValue){
-    console.log(formValue);
-  };
-  ngAfterViewInit(){
-    Observable.combineLatest(
-      this.form.statusChanges,
-      this.form.valueChanges,
-      (status, value) => ({status, value})
-    )
-      .filter(({status}) => status === 'VALID' )
-      .subscribe(({value}) => console.table(value));
-  }
+locations = ['home', 'office', 'theater', 'restaurant'];
+
 }
